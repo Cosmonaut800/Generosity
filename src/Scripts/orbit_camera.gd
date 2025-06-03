f72ae4185fbcbad6e1538f78d7f2cb5a631318ac
Sean Miller <seancm800@gmail.com>
2025-06-03 02:07:00 -0400

    Added quest giver models

--- src/Scripts/orbit_camera.gd
+++ src/Scripts/orbit_camera.gd
@@ -19,7 +19,7 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(event):
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("toggle_mouse"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else:

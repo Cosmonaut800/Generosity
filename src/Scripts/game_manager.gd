3b27650a795f381b63fa78f584f9955cc5b08547
Sean Miller <seancm800@gmail.com>
2025-06-08 19:17:29 -0400

    Add music fading

--- src/Scripts/game_manager.gd
+++ src/Scripts/game_manager.gd
@@ -1,3 +1,14 @@
 class_name GameManager extends Node
 
 var kodama_count := 0
+var kodama_level := 0
+
+func evaluate_kodama_level():
+	if kodama_count < 1:
+		kodama_level = 0
+	elif kodama_count >= 1:
+		kodama_level = 1
+	elif kodama_count >= 3:
+		kodama_level = 2
+	elif kodama_count >= 6:
+		kodama_level = 3
